32054ad28d9913e1cd8bae36c5f96208fcdf1f2f
Sam <donahsan2854@gmail.com>
2022-01-24 22:23:03 +0600

    My Project on Covid 19

---
+++ lib/info_screen.dart
@@ -0,0 +1,255 @@
+import 'package:covid_19/const.dart';
+import 'package:covid_19/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+
+class InfoScreen extends StatefulWidget {
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            MyHeader(
+              image: "assets/icons/coronadr.svg",
+              textTop: "Covid-19",
+              textBottom: "Details",
+              iconleft: true,
+              offset: offset,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Symptoms",
+                    style: dTitleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          SymptomCard(
+                            image: "assets/images/headache.png",
+                            title: "Headache",
+                            // isActive: true,
+                          ),
+                          SizedBox(width: 15),
+                          SymptomCard(
+                            image: "assets/images/cough.png",
+                            title: "Cough",
+                          ),
+                          SizedBox(width: 15),
+                          SymptomCard(
+                            image: "assets/images/fever.png",
+                            title: "Fever",
+                          ),
+                          SizedBox(width: 15),
+                          SymptomCard(
+                            image: "assets/images/headache.png",
+                            title: "Tiredness",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text("Prevention", style: dTitleTextstyle),
+                  SizedBox(height: 20),
+                  PreventCard(
+                    text:
+                        "Avoid going to crowded places where people come together in crowds.",
+                    image: "assets/images/stayhome.png",
+                    title: "STAY HOME \nSave Lives",
+                  ),
+                  PreventCard(
+                    text:
+                        "Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze.",
+                    image: "assets/images/covercough.png",
+                    title: "COVER \nyour cough",
+                  ),
+                  PreventCard(
+                    text:
+                        "Maintain a safe distance (at least 1 meter) from anyone who is coughing or sneezing.",
+                    image: "assets/images/distance.png",
+                    title: "KEEP \nsafe distance",
+                  ),
+                  PreventCard(
+                    text:
+                        "Clean your hands often. Use soap and water, or an alcohol-based hand sanitizer.",
+                    image: "assets/images/washhands.png",
+                    title: "WASH \nhands often",
+                  ),
+                  PreventCard(
+                    text:
+                        "If you have a fever, a cough, and difficulty breathing, seek medical attention. CALL !",
+                    image: "assets/images/sick.png",
+                    title: "SICK? \nCall helpline",
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String text;
+  const PreventCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 160,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              height: 135,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 8),
+                    blurRadius: 24,
+                    color: dShadowColor,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              image,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              left: 150,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                height: 145,
+                width: MediaQuery.of(context).size.width - 170,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: dTitleTextstyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        text,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    // Align(
+                    //   alignment: Alignment.topRight,
+                    //   child: SvgPicture.asset("assets/icons/forward.svg"),
+                    // ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SymptomCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final bool isActive;
+  const SymptomCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          isActive
+              ? BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 20,
+                  color: dActiveShadowColor,
+                )
+              : BoxShadow(
+                  offset: Offset(0, 3),
+                  blurRadius: 6,
+                  color: dShadowColor,
+                ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.asset(image, height: 90),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,283 @@
+import 'package:covid_19/const.dart';
+import 'package:covid_19/provider/data.dart';
+import 'package:covid_19/widgets/counter.dart';
+import 'package:covid_19/widgets/details_screen.dart';
+import 'package:covid_19/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          scaffoldBackgroundColor: dBackgroundColor,
+          fontFamily: "Poppins",
+          textTheme: TextTheme(
+            bodyText1: TextStyle(color: dBodyTextColor),
+          )),
+      home: MyHomePage(),
+      // home: DetailsScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = ScrollController();
+  double offset = 0;
+  final String formattedDate = DateFormat.yMMMMEEEEd().format(DateTime.now());
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<Album>(
+        future: futureAlbum,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
+              controller: controller,
+              child: Column(
+                children: <Widget>[
+                  MyHeader(
+                    image: "assets/icons/stayHome.svg",
+                    iconleft: false,
+                    textTop: "Stay\nHome.",
+                    textBottom: "Stay\nSafe.",
+                    offset: offset,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              "COVID-19 Cases Overview",
+                              style: dHeadingTextStyle,
+                            ),
+                            SizedBox(
+                              height: 3.0,
+                            ),
+                            Text(
+                              "Updated: " + formattedDate,
+                              style: TextStyle(
+                                color: dTextLightColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 4),
+                                blurRadius: 30,
+                                color: dShadowColor,
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Counter(
+                                    color: Colors.deepOrange.shade500,
+                                    number: snapshot.data!.totalConfirmed,
+                                    title: "Total Confirmed",
+                                  ),
+                                  Spacer(),
+                                  Counter(
+                                    color: dInfectedColor,
+                                    number: snapshot.data!.newConfirmed,
+                                    title: "New Confirmed",
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20.0),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Counter(
+                                    color: Colors.red.shade900,
+                                    number: snapshot.data!.totalDeaths,
+                                    title: "Total Deaths",
+                                  ),
+                                  SizedBox(width: 20.0),
+                                  Counter(
+                                    color: Colors.redAccent.shade400,
+                                    number: snapshot.data!.newDeaths,
+                                    title: "New Deaths",
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20.0),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Counter(
+                                    color: Colors.green.shade800,
+                                    number: snapshot.data!.totalRecovered,
+                                    title: "Total Recovered",
+                                  ),
+                                  Spacer(),
+                                  Counter(
+                                    color: dRecovercolor,
+                                    number: snapshot.data!.newRecovered,
+                                    title: "New Recovered",
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Mapping the Spread",
+                              style: dTitleTextstyle,
+                            ),
+                            TextButton(
+                              child: Text(
+                                "View Details",
+                                style: TextStyle(
+                                  color: dPrimaryColor,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return DetailsScreen();
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20.0, bottom: 30.0),
+                          padding: EdgeInsets.all(5),
+                          height: 178,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 20,
+                                color: dShadowColor,
+                              ),
+                            ],
+                          ),
+                          child: Image.asset(
+                            "assets/images/map.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return AlertDialog(
+              title: Text(
+                'An Error Occured!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
+              ),
+              content: Text(
+                "${snapshot.error}",
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text(
+                    'Close',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  onPressed: () {
+                    SystemNavigator.pop();
+                  },
+                )
+              ],
+            );
+            // Text(snapshot.data.title.toString()),
+          }
+          // By default, show a loading spinner.
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(height: 20),
+                Text('Getting the latest data.')
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
